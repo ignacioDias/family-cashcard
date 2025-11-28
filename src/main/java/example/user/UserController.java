c33fd261ec09a648874762457990995b5fde9756
Ignacio Dias Gundin <ignadiasg@gmail.com>
2025-11-28 14:59:37 -0300

    bugs

--- src/main/java/example/user/UserController.java
+++ src/main/java/example/user/UserController.java
@@ -3,11 +3,15 @@ package example.user;
 import java.net.URI;
 import java.security.Principal;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -25,12 +29,19 @@ public class UserController {
 
     @PostMapping("/register")
     private ResponseEntity<Void> register(@RequestBody User user, UriComponentsBuilder ucb) {
-        if(user == null || user.getUsername() == null || user.getPassword() == null || user.getUsername().isBlank() || user.getPassword().isBlank()) { 
+        if (user == null 
+                || user.getUsername() == null 
+                || user.getPassword() == null 
+                || user.getUsername().isBlank() 
+                || user.getPassword().isBlank()) {
+
             return ResponseEntity.badRequest().build();
         }
+
         if (userRepository.findById(user.getUsername()).isPresent()) {
-            return ResponseEntity.badRequest().build();
+            return ResponseEntity.status(HttpStatus.CONFLICT).build(); 
         }
+
         var password = passwordEncoder.encode(user.getPassword());
         var registeredUser = new User(user.getUsername(), password);
         userRepository.save(registeredUser);
@@ -40,5 +51,28 @@ public class UserController {
                 .buildAndExpand(registeredUser.getUsername())
                 .toUri();
         return ResponseEntity.created(locationOfNewCashCard).build();
+    }
+
+   @PutMapping("/{username}/change-password")
+    private ResponseEntity<Void> changePassword(@PathVariable String username, @RequestBody ChangePasswordRequest request, Principal principal) {
+        
+        var currentPassword = request.getCurrentPassword();
+        var newPassword = request.getNewPassword();
+        
+        if(!principal.getName().equals(username))
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        var userFromDB = userRepository.findById(username)
+                .orElse(null);
+        if(userFromDB == null)
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        if (!passwordEncoder.matches(currentPassword, userFromDB.getPassword())) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        var hashedNewPassword = passwordEncoder.encode(newPassword);
+        var updatedUser = new User(username, hashedNewPassword);
+
+        userRepository.save(updatedUser);
+
+        return ResponseEntity.noContent().build();
     }
 }
