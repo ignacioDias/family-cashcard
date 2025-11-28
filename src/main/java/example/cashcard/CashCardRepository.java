624d771c58c70d29654b2a4f09977094789ae7a8
Ignacio Dias Gundin <ignadiasg@gmail.com>
2025-11-28 11:18:14 -0300

    changes in db + user classes

--- src/main/java/example/cashcard/CashCardRepository.java
+++ src/main/java/example/cashcard/CashCardRepository.java
@@ -1,12 +1,14 @@
 package example.cashcard;
 
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CashCardRepository extends CrudRepository<CashCard, Long>, PagingAndSortingRepository<CashCard, Long> {
+public interface CashCardRepository extends JpaRepository<CashCard, Long> {
+
     CashCard findByIdAndOwner(Long id, String owner);
+
     boolean existsByIdAndOwner(Long id, String owner);
-    Page<CashCard> findByOwner(String name, PageRequest pageRequest);
+
+    Page<CashCard> findByOwner(String owner, Pageable pageable);
 }
---
+++ src/main/java/example/user/User.java
@@ -0,0 +1,61 @@
+package example.user;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private String username;
+    private String password;
+    private String role;
+
+    public User( ) {
+    }
+
+    public User(String username, String password, String role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return this.role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(this.username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, password, role);
+    }
+    
+}
